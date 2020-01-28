7f3b6df7d09c3d389458d74698182529afa3327b
William Wainwright <51410426+wjwainwright@users.noreply.github.com>
2020-01-28 14:04:39 -0500

    comments

--- interpolate.py
+++ interpolate.py
@@ -17,19 +17,24 @@ def linearInterpolate(xarr,yarr):
     def subFunc(x):
         import numpy as np
         
+        #Checks boundary conditions
         if x < xarr[0] or x > xarr[-1]:
             print("X position out of bounds")
             return
         
+        #Gets nearest value in the xarr to sample x
         arr = np.asarray(xarr) 
         a = arr[(np.abs(arr - x)).argmin()]
         
+        #Assigns x0 and x1 based on a
         if a < x:
             x0 = a
             x1 = xarr[xarr.index(a)+1]
         else:
             x1 = a
             x0 = xarr[xarr.index(a)-1]
+        
+        #Assigns y0,y1,xd
         y0 = yarr[xarr.index(x0)]
         y1 = yarr[xarr.index(x1)]
         xd = (x-x0)/(x1-x0)
