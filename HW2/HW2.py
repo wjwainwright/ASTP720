ca655b36e7fd04e3cf69abd6394fc37888096d9a
William Wainwright <51410426+wjwainwright@users.noreply.github.com>
2020-02-10 17:21:24 -0500

    HW2 Matrix stuff
    WIP

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -2,18 +2,20 @@
 
 import numpy as np
 import calculus as calc
+from matrix import matrix
 
 def testFunc(x):
     return 2*x**3 - 3*x + 7
 
 #Number 1 test
+print("Number 1 Test:")
 xarr = list(np.arange(0,20,0.1))
 xarr = [round(x,4) for x in xarr]
 yarr = [testFunc(x) for x in xarr]
 
 #Symmetric difference derivative
 diff = calc.differentiate(xarr,yarr)
-print(diff(19.9))
+print(diff(19.5))
 
 a=-4
 b=-20
@@ -29,3 +31,29 @@ print(trap(a,b))
 #Simpson Integral
 sim = calc.simpsonInt(testFunc)
 print(sim(a,b))
+
+
+
+
+
+#Number 3 test
+print("Number 3 Test:")
+#m = matrix(9,9,source='A_coefficients.dat',mode='file')
+m = np.empty((0,4))
+m = np.r_[m,[[2,10,6,9]]]
+m = np.r_[m,[[3,-1,4,20]]]
+m = np.r_[m,[[5,0,3,-11]]]
+m = np.r_[m,[[-7,8,0,2]]]
+
+m = matrix(4,4,m,mode='numpy')
+
+print(m.values)
+summed = m+m
+print(summed.values)
+transpose = m.transpose()
+print(transpose.values)
+mult = m*m
+print(mult.values)
+print(m.trace())
+
+print(m.determinant())
---
+++ HW2/matrix.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+class matrix:
+    
+    def __init__(self,m,n,source='none',mode='none'):
+        self.m = int(m)
+        self.n = int(n)
+        self.values = np.zeros((self.m,self.n))
+        
+        if not mode == 'none':
+             
+            if mode == 'numpy':
+                #Importing a numpy matrix to my matrix format (which is still a numpy matrix under the hood)
+                self.values = source
+            else:
+                file = open(source).read()
+                lines = file.split('\n')
+                lines.pop(0)
+                
+                for line in lines:
+                    line = line.strip()
+                    val = line.split(',')
+                    
+                    r = int(val[0]) - 1
+                    c = int(val[1]) - 1
+                    self.values[r,c] = float(val[2])
+                
+    def __add__(self,other):
+        
+        try:
+            assert self.m == other.m
+            assert self.n == other.m
+        except:
+            print("Matrices must be the same shape to add")
+            raise
+        
+        
+        ans = matrix(self.m,self.n)
+        
+        for r,row in enumerate(self.values):
+            for c,val in enumerate(row):
+                ans.values[r,c] = val + other.values[r,c]
+        return ans
+    
+    def __sub__(self,other):
+        
+        try:
+            assert self.m == other.m
+            assert self.n == other.m
+        except:
+            print("Matrices must be the same shape to subtract")
+            raise
+        
+        
+        ans = matrix(self.m,self.n)
+        for r,row in enumerate(self.values):
+            for c,val in enumerate(row):
+                ans.values[r,c] = val - other.values[r,c]
+        return ans
+    
+    def __mul__(self,other):
+        try:
+            assert self.n == other.m
+        except:
+            print("Matrices must have compatible dimensions for multiplication. M1_cols == M2_rows")
+            raise
+        
+        ans = matrix(self.m,other.n)
+        
+        for r,row in enumerate(self.values):
+            for c,col in enumerate(other.transpose().values):
+                ans.values[r][c] = sum([a*b for a,b in zip(row,col)])
+                
+        return ans
+
+    def transpose(self):
+        ans = matrix(self.n,self.m)
+        
+        for r,row in enumerate(self.values):
+            for c,val in enumerate(row):
+                ans.values[c,r] = val
+        return ans
+    
+    def trace(self):
+        try:
+            assert self.m == self.n
+        except:
+            print("Matrix must be square")
+            raise
+        
+        return sum([self.values[i,i] for i in range(self.m)])
+    
+    def determinant(self):
+        try:
+            assert self.m == self.n
+        except:
+            print("Matrix must be square")
+            raise
+        
+        if self.m == 2:
+            return self.values[0,0]*self.values[1,1] - self.values[0,1]*self.values[1,0]
+        
+        det = []
+        for c,val in enumerate(self.values[0]):
+            det.append( (-1)**c * val * self.residual(0,c).determinant() )
+        return sum(det)
+
+    def residual(self,r,c):
+        ans = np.delete(self.values,r,0)
+        ans = np.delete(ans,c,1)
+        return matrix(self.m-1,self.n-1,ans,mode='numpy')
