70c53f19e9b08f2600d8b18aa381549ef2bb55ab
William Wainwright <51410426+wjwainwright@users.noreply.github.com>
2020-02-21 15:52:58 -0500

    hw3 progress

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import ODE
+
+def fPrime(x,y1,y2):
+    #print(f"{x}   {y1}   {y2}")
+    return 3*x**2+8*y1-3*y2
+
+def fPrime2(x,y):
+    return 4*x+5*y+1
+
+solvePoints = [0,1,2,3]
+
+#Euler
+print(ODE.euler(fPrime,0,[-1,0],solvePoints))
+print(ODE.euler(fPrime2,0,[1],solvePoints))
+
 
 
--- HW3/ODE.py
+++ HW3/ODE.py
@@ -8,16 +8,66 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return array[idx]
 
-def euler(func,x0,y0,solvePoints,step=0.05):
+def euler(func,x0,y0,solvePoints,precision=3,full=False):
     import numpy as np
     
+    step = 10**(-1*precision)
+    
     solvePoints = sorted(solvePoints)
-    x = np.arange(solvePoints[0],solvePoints[-1]+step,step)
+    x = np.arange(x0,solvePoints[-1]+step,step)
+    yprime = []
+    y = [y0[0]]
+    ans = []
+    
+    x = [round(a,precision) for a in x]
+    
+    if not len(y0) == 1:
+        yprime.append(y0[1])
+    
+    for i in range(len(x)-1):
+        if len(y0) ==1:
+            yprime.append(func(x[i],y[i]))
+        else:
+            yprime.append(yprime[i] + step*func(x[i],y[i],yprime[i]))
+        y.append(y[i]+step*yprime[-1])
     
     
+    y = [round(a,precision) for a in y]
+    yprime = [round(a,precision) for a in yprime]
     
+    for val in solvePoints:
+        ans.append(y[x.index(val)])
     
-    
-    return
+    if full:
+        return x,y,yprime
+    else:
+        return ans
 
-
+def heun(func,x0,y0,solvePoints,precision=3,full=False):
+    import numpy as np
+    
+    step = 10**(-1*precision)
+    
+    solvePoints = sorted(solvePoints)
+    x = np.arange(x0,solvePoints[-1]+step,step)
+    yprime = []
+    y = [y0[0]]
+    ans = []
+    
+    x = [round(a,precision) for a in x]
+    
+    if not len(y0) == 1:
+        yprime.append(y[1])
+    
+    for i in range(len(x)-1):
+        
+        if len(y0) == 1:
+            p = y[i] + step*func(x[i],y[i])
+            c = y[i] + 0.5*step*(func(x[i],y[i]) + func(x[i+1],p))
+            y.append(c)
+            yprime.append(func(x[i],y[i]))
+        else:
+            p = y[i] + step*func(x[i],y[i],yprime[i])
+            p2 = yprime[i] + step*func(x[i],y[i],yprime[i])
+            
+            c = y[i] + 0.5*step*(func(x[i],y[i],yprime[i]) + func(x[i+1],p,p2)
