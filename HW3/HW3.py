3ea2184255444322ddb38316a2640d957f2dc231
William Wainwright <51410426+wjwainwright@users.noreply.github.com>
2020-02-23 18:51:56 -0500

    Finished problem 1

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -2,18 +2,23 @@
 
 import ODE
 
-def fPrime(x,y1,y2):
+def f2Prime(x,y1,y2):
     #print(f"{x}   {y1}   {y2}")
-    return 3*x**2+8*y1-3*y2
+    return [3*x**2+8*y1-3*y2]*2
 
-def fPrime2(x,y):
-    return 4*x+5*y+1
+def fPrime(x,y):
+    return [5*y+1]
 
 solvePoints = [0,1,2,3]
 
 #Euler
-print(ODE.euler(fPrime,0,[-1,0],solvePoints))
-print(ODE.euler(fPrime2,0,[1],solvePoints))
+print(ODE.euler(f2Prime,0,[-1,0],solvePoints))
+print(ODE.euler(fPrime,0,[-1],solvePoints))
 
+#Heun
+print(ODE.heun(f2Prime,0,[-1,0],solvePoints))
+print(ODE.heun(fPrime,0,[-1],solvePoints))
 
-
+#RK4
+print(ODE.rk4(f2Prime,0,[-1,0],solvePoints))
+print(ODE.rk4(fPrime,0,[-1],solvePoints))
--- HW3/ODE.py
+++ HW3/ODE.py
@@ -26,9 +26,9 @@ def euler(func,x0,y0,solvePoints,precision=3,full=False):
     
     for i in range(len(x)-1):
         if len(y0) ==1:
-            yprime.append(func(x[i],y[i]))
+            yprime.append(func(x[i],y[i])[0])
         else:
-            yprime.append(yprime[i] + step*func(x[i],y[i],yprime[i]))
+            yprime.append(yprime[i] + step*func(x[i],y[i],yprime[i])[0])
         y.append(y[i]+step*yprime[-1])
     
     
@@ -57,17 +57,89 @@ def heun(func,x0,y0,solvePoints,precision=3,full=False):
     x = [round(a,precision) for a in x]
     
     if not len(y0) == 1:
-        yprime.append(y[1])
+        yprime.append(y0[1])
     
     for i in range(len(x)-1):
         
         if len(y0) == 1:
-            p = y[i] + step*func(x[i],y[i])
-            c = y[i] + 0.5*step*(func(x[i],y[i]) + func(x[i+1],p))
+            p = y[i] + step*func(x[i],y[i])[0]
+            c = y[i] + 0.5*step*(func(x[i],y[i])[0] + func(x[i+1],p)[0])
             y.append(c)
-            yprime.append(func(x[i],y[i]))
+            yprime.append(func(x[i],y[i])[0])
         else:
-            p = y[i] + step*func(x[i],y[i],yprime[i])
-            p2 = yprime[i] + step*func(x[i],y[i],yprime[i])
+            p = y[i] + step*func(x[i],y[i],yprime[i])[0]
+            p2 = yprime[i] + step*func(x[i],y[i],yprime[i])[1]
             
-            c = y[i] + 0.5*step*(func(x[i],y[i],yprime[i]) + func(x[i+1],p,p2)
+            c = y[i] + 0.5*step*(func(x[i],y[i],yprime[i])[0] + func(x[i+1],p,p2)[0])
+            c2 = yprime[i] + 0.5*step*(func(x[i],y[i],yprime[i])[1] + func(x[i+1],p,p2)[1])
+            
+            y.append(c)
+            yprime.append(c2)
+            
+    y = [round(a,precision) for a in y]
+    yprime = [round(a,precision) for a in yprime]
+    
+    for val in solvePoints:
+        ans.append(y[x.index(val)])
+    
+    if full:
+        return x,y,yprime
+    else:
+        return ans
+
+
+def rk4(func,x0,y0,solvePoints,precision=3,full=False):
+    import numpy as np
+    
+    step = 10**(-1*precision)
+    
+    solvePoints = sorted(solvePoints)
+    x = np.arange(x0,solvePoints[-1]+step,step)
+    yprime = []
+    y = [y0[0]]
+    ans = []
+    
+    x = [round(a,precision) for a in x]
+    
+    if not len(y0) == 1:
+        yprime.append(y0[1])
+    
+    for i in range(len(x)-1):
+        
+        if len(y0) == 1:
+            
+            k1 = func(x[i],y[i])[0]
+            k2 = func(x[i]+0.5*step,y[i]+0.5*step*k1)[0]
+            k3 = func(x[i]+0.5*step,y[i]+0.5*step*k2)[0]
+            k4 = func(x[i]+0.5*step,y[i]+0.5*step*k3)[0]
+            
+            y.append(y[i]+step/6*(k1+2*k2+2*k3+k4))
+            yprime.append(k1)
+            
+        else:
+            
+            k1 = func(x[i],y[i],yprime[i])[0]
+            k1p = func(x[i],y[i],yprime[i])[1]
+            
+            k2 = func(x[i]+0.5*step,y[i]+0.5*step*k1,yprime[i]+0.5*step*k1p)[0]
+            k2p = func(x[i]+0.5*step,y[i]+0.5*step*k1,yprime[i]+0.5*step*k1p)[1]
+            
+            k3 = func(x[i]+0.5*step,y[i]+0.5*step*k2,yprime[i]+0.5*step*k2p)[0]
+            k3p = func(x[i]+0.5*step,y[i]+0.5*step*k2,yprime[i]+0.5*step*k2p)[1]
+            
+            k4 = func(x[i]+0.5*step,y[i]+0.5*step*k3,yprime[i]+0.5*step*k3p)[0]
+            k4p = func(x[i]+0.5*step,y[i]+0.5*step*k3,yprime[i]+0.5*step*k3p)[1]
+            
+            y.append(y[i]+step/6*(k1+2*k2+2*k3+k4))
+            yprime.append(yprime[i]+step/6*(k1+2*k2p+2*k3p+k4p))
+    
+    y = [round(a,precision) for a in y]
+    yprime = [round(a,precision) for a in yprime]
+    
+    for val in solvePoints:
+        ans.append(y[x.index(val)])
+    
+    if full:
+        return x,y,yprime
+    else:
+        return ans
